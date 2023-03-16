b34a51e49b6c437647f940e53924951fc7f8d0a6
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-17 01:24:48 +0200

    Updating 4-puts file - add some description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - Entry Point
+*
+* Description: The file should print a message using puts funtion
+*
+* Return: The main function should return 0 (success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
