e7905a1957e7a99fc8aa8601d604d62d7e906670
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-16 16:34:20 -0700

    Creating 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function should print a message using printf function
+ *
+ * Return: the main function should return 0 (success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
