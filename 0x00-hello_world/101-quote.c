072643691400be42a50bba843a5fa0c3fd51103d
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-16 17:13:40 -0700

    Creating 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function should print a message
+ *
+ * Return: The main function should return 1 (error)
+*/
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
