a5922ae18d2c0a499617ce5a61c5c7c22b3758c9
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-18 00:14:27 +0200

    Creating 1-last_digit.c file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function gets a random number and check its last digit
+ * is greater than 5 or less than 6 and not 0 or is equal to 0
+ *
+ * Return: The main function should return 0 (success)
+*/
+
+int main(void)
+{
+int n, lastDigit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+
+if (lastDigit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+}
+if (lastDigit < 6 && lastDigit != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+}
+if (lastDigit == 0)
+{
+printf("Last digit of %d is %d and is zero\n", n, lastDigit);
+}
+
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 # Second lesson
 
 - First C file is 0-positive_or_negative.c it generates a random number and check its value is positive or negative or equal to zero.
+
+- Second C file is 1-last_digit.c it generates a random number and check its last digit is greater than 5 or less than 6 and not 0 or is equal to 0.
