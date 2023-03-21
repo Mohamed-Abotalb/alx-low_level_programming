5bb2c77d566575ff262f36fe41448849ba1a22b8
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-22 00:06:12 +0200

    Creating 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function prints "_putchar" followed by a new line.
+ *
+ * Return: The main function should return 0 (success)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# Third lesson
+
+## Basic Parts of the Task
+
+- First C file is 0-putchar.c, it prints "\_putchar" followed by a new line.
