9cec97c7c0943585c8a09fdd86ecc9727fcdf0cf
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-22 01:01:57 +0200

    Updating 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,5 @@
 #include "main.h"
 
-/* more headers goes there */
-
-/**
- * main - Entry point
- *
- * Description: The main function prints the alphabet, in lowercase,
- * followed by a new line using _putchar prototype.
- *
- * Return: The main function should return 0 (success)
-*/
-
-int main(void)
-{
-print_alphabet();
-
-return (0);
-}
-
 /**
  * print_alphabet - prints the alphabet, in lowercase from 'a' to 'z'
  *  followed by a new line.
