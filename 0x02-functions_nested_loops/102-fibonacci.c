e7d3950385b58024d44cc68d15812f00cb010f6a
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-23 00:58:25 +0200

    Creating 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: The main function should return 0 (success)
+*/
+
+int main(void)
+{
+unsigned long num1 = 1, num2 = 2, num3, index;
+
+printf("%lu, %lu, ", num1, num2);
+for (index = 2; index <= 50; index++)
+{
+num3 = num1 + num2;
+printf("%lu", num3);
+if (index != 50)
+{
+printf(", ");
+}
+num1 = num2;
+num2 = num3;
+}
+putchar('\n');
+
+return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -31,3 +31,5 @@
 - First C file is 100-times_table.c, it prints the n times table, starting with 0, if n is greater than 15 or less than 0 the function should not print anything.
 
 - Second C file is 101-natural.c, it computes and prints the sum of all the multiples of 3 or 5 below 1024.
+
+- Third C file is 102-fibonacci.c, it prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
