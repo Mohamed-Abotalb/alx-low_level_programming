52b067881b8e2266c16f42d644a14057087c7699
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-23 02:23:47 +0200

    Updating 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 /**
+ * numLength - It is a function returns the length of a fibonacci number
+ *
+ * @num: checks for input of fibonacci number
+ *
+ * Return: The function should return the number of digits
+*/
+
+int numLength(int num)
+{
+int length = 0;
+
+if (!num)
+{
+return (1);
+}
+while (num)
+{
+num /= 10;
+length++;
+}
+return (length);
+}
+
+/**
  * main - Entry point
  *
  * Description: The main function prints the first 98 Fibonacci numbers,
@@ -11,19 +35,35 @@
 
 int main(void)
 {
-unsigned long num1 = 1, num2 = 2, num3, index;
+int count, initialZeros;
+unsigned long num1 = 1, num2 = 2, num3, zero1 = 0, zero2 = 0,
+zero3 = 0, max = 100000000;
 
-printf("%lu, %lu, ", num1, num2);
-for (index = 3; index <= 98; index++)
+
+for (count = 1; count < 98; count++)
 {
-num3 = num1 + num2;
-printf("%lu", num3);
-if (index != 98)
+if (zero1 > 0)
+{
+printf("%lu", zero1);
+}
+initialZeros = numLength(max) - 1 - numLength(num1);
+
+while (zero1 > 0 && initialZeros > 0)
+{
+printf("%d", 0);
+initialZeros--;
+}
+printf("%lu", num1);
+num3 = (num1 + num2) % max;
+zero3 = zero1 + zero2 + (num1 + num2) / max;
+num1 = num2;
+zero1 = zero2;
+num2 = num3;
+zero2 = zero3;
+if (count != 98)
 {
 printf(", ");
 }
-num1 = num2;
-num2 = num3;
 }
 putchar('\n');
 
