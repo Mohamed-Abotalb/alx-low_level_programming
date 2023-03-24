f00d21fa16c3f450554188c4d82a5abf6350458b
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-24 02:11:38 +0200

    Creating 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ *
+ * @c: checks input of the function.
+ *
+ * Return: The main function should return 0 (success).
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,5 @@
 ## Basic Parts of the Task
 
 - First C file is 0-isupper.c, it checks for uppercase character.
+
+- Second C file is 1-isdigit.c, it checks for a digit (0 through 9).
