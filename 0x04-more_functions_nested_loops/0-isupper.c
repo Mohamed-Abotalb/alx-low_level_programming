88ed02f6e37328c656bd7da9c263f40dc066c789
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-24 02:08:18 +0200

    Creating 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: checks input of the function.
+ *
+ * Return: The main function should return 0 (success).
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# Fifth Lesson
+
+## Basic Parts of the Task
+
+- First C file is 0-isupper.c, it checks for uppercase character.
