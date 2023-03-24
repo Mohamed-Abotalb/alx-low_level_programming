c70f6751c882060e2eadc3b4e9417a208c897cee
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-24 03:08:19 +0200

    Updating 6-print_line.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,8 @@
 /**
  * print_line - draws a straight line in the terminal.
  *
+ * @n: checks input of the function.
+ *
  * Return: void.
 */
 
@@ -11,13 +13,13 @@ void print_line(int n)
 int num;
 if (n <= 0)
 {
-    _putchar('\n');
+_putchar('\n');
 }
 else
 {
 for (num = 0; num < n; num++)
 {
-    _putchar('_');
+_putchar('_');
 }
 _putchar('\n');
 }
