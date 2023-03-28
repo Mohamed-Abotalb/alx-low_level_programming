c2f209ad556e892949c4d91c7b05fd360f679524
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-28 03:18:49 +0200

    Creating 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * @n: checks input of the function.
+ *
+ * Return: The main function should return 0 (success).
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+# Fifth Lesson
+
+## Basic Parts of the Task
+
+- First C file is 0-reset_to_98.c, it takes a pointer to an 'int' as parameter and updates the value it points to to 98.
