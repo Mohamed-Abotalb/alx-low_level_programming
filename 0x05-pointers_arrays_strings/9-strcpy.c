cc7ba2c0f7fba7a39fcf80c5c7018de81e9f4a7c
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-28 04:59:37 +0200

    Updating 9-strcpy.c file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,12 +14,10 @@ char *_strcpy(char *dest, char *src)
 {
 int count = -1;
 
-do
-{
+do {
 count++;
 dest[count] = src[count];
-}
-while (src[count] != '\0');
+} while (src[count] != '\0');
 
 return (dest);
 }
