6e49f15494af1fe982b56bf6e41143360eff08f4
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-29 04:07:54 +0200

    Updating 101-keygen.c file :

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,21 +8,21 @@
  * description: The main function should generate a random password
  * for the program 101-crackme.
  *
- * Return: The function should return the inter value;
+ * Return: The function should return 0 (success);
 */
 
 int main(void)
 {
-int sum;
-char c;
+    int sum;
+    char c;
 
-srand(time(NULL));
-while (sum <= 2645)
-{
-c = rand() % 128;
-sum += c;
-putchar(c);
-}
-putchar(2772 - sum);
-return (0);
+    srand(time(NULL));
+    while (sum <= 2645)
+    {
+        c = rand() % 128;
+        sum += c;
+        putchar(c);
+    }
+    putchar(2772 - sum);
+    return (0);
 }
