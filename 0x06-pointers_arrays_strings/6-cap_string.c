4b05808de55442c3b59a71cd676ab7ab65f3b95b
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-31 04:22:43 +0200

    Updating 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 
 /**
+ * isLower - it determins if the character is in lowercase.
+ *
+ * @c: checks input of the function.
+ *
+ * Return: The function should return 1 if true or 0 if false.
+*/
+
+int isLower(char c)
+{
+    return (c >= 'a' && c <= 'z');
+}
+
+/**
  * isSeparator - it determins which character is a separator.
  *
  * @c: checks input of the function.
@@ -16,11 +29,14 @@ char separator[] = " \t\n.,!?\"(){}";
 
 for (count = 0; separator[count] < 12; count++)
 {
+
 if (c == separator[count])
 {
 return (1);
 }
+
 }
+
 return (0);
 }
 
@@ -40,19 +56,23 @@ int foundSeparator = 1;
 
 while (*str)
 {
+
 if (isSeparator(*str))
 {
 foundSeparator = 1;
 }
+
 else if (isLower(*str) && foundSeparator)
 {
 *str -= 32;
 foundSeparator = 0;
 }
+
 else
 {
 foundSeparator = 0;
 }
+
 str++;
 }
 
