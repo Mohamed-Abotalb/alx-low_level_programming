45ee3792794fe292244f655929041c7e0b9bdd20
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-03-31 03:41:57 +0200

    Updating 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: checks input of the function.
  * @s2: checks input of the function.
  *
- * Return: The function should return the pointer to dest.
+ * Return: The function should return 1 if true or 0 is false.
 */
 
 int _strcmp(char *s1, char *s2)
@@ -17,11 +17,11 @@ while (*s1)
 {
 if (*s1 != *s2)
 {
-equal = ((int) *s1 - 48) - ((int) *s2 - 48);
+equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 break;
 }
-*s1++;
-*s2++;
+s1++;
+s2++;
 }
 return (equal);
 
