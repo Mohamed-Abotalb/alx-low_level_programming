54b66b0b440c0fe87cc2ebe3f38b66f94be32dbe
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-06 05:01:38 +0200

    Updating 5-sqrt_recursion.c file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,10 +4,11 @@
  * _sqrt_recursion - it returns the natural square root of a number.
  *
  * @n: checks input of the function.
+ * @root: checks input of the function.
  *
  * Return: The function should return the natural square root of the number.
 */
-
+int square(int n, int root);
 int _sqrt_recursion(int n)
 {
 return (square(n, 1));
