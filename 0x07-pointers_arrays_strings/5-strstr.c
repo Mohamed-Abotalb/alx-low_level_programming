e0046d3a9c2b973de83fffd6ce62970a7dab75b6
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-14 16:35:28 +0200

    Updating 5-strstr.c file'

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * _strpbrk - it locates a substring.
+ * *_strstr - it locates a substring.
  *
  * @haystack: checks input of the function.
  * @needle: checks input of the function.
  *
- * Return: The function should return the pointer to the beginning of the located substring.
+ * Return: The function should return 0 (success).
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-int count;
-
-for (count = 0; haystack[count] >= '\0'; count++)
+for (; *haystack != '\0'; haystack++)
 {
+char *l = haystack;
+char *p = needle;
 
-if (haystack[count] == needle)
+while (*l == *p && *p != '\0')
 {
-return (haystack + count);
+l++;
+p++;
 }
 
+if (*p == '\0')
+return (haystack);
 }
 
-return ('\0');
+return (0);
 }
