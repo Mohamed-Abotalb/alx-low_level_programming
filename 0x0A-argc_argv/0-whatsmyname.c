02463f9091d9e3fea74671ba4465f3339b8a2fc8
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-16 02:57:06 +0200

    Creating 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - it prints the program name, followed by a new line..
+ *
+ * @argc: checks input of the function.
+ * @argv: checks input of the function.
+ *
+ * Return: The function should return (0) success.
+*/
+
+int main(int argc, char const *argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+# Tenth Lesson
+
+## Basic Parts of the Task
+
+- First C file is 0-whatsmyname.c, it prints the program name, followed by a new line.
