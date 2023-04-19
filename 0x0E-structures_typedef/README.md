1b3e5d88a3f3102b982b8603da8766117490349d
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-19 02:40:39 +0200

    Creating dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# Fourteenth Lesson
+
+## Basic Parts of the Task
+
+- First file is dog.h, it Define a new type struct dog with the following elements:
+  name, type = char\*
+  age, type = float
+  owner, type = char\*
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct.
+ *
+ * @name: dog's name,
+ * @age: dog's age,
+ * @owner: dog's owner
+ *
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
