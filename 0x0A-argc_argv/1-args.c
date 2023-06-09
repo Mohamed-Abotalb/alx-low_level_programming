d6b55164d2b4e480c780f653ef1ab5938051c58e
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-06-09 23:43:39 +0300

    Creating 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - it prints the number of arguments passed into it,
+ * followed by a new line.
+ *
+ * @argc: checks input of the function.
+ * @argv: checks input of the function.
+ *
+ * Return: The function should return (0) success.
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
