51f45ecc762082cee9b9f2c08393140610656c02
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-06-09 23:45:11 +0300

    Creating 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - it it adds positive numbers and prints the result,
+ * followed by a new line.
+ *
+ * @argc: checks input of the function.
+ * @argv: checks input of the function.
+ *
+ * Return: The function should return (0) success.
+*/
+
+int main(int argc, char *argv[])
+{
+int result = 0;
+char *character;
+
+while (--argc)
+{
+
+for (character = argv[argc]; *character; character++)
+{
+
+if (*character < '0' || *character > '9')
+{
+printf("Error\n");
+return (1);
+}
+
+}
+
+result += atoi(argv[argc]);
+}
+
+printf("%d\n", result);
+
+return (0);
+}
